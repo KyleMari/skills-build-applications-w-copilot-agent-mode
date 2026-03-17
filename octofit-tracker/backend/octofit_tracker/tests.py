e73cec0628234981466a3fbb16826b36ca510267
KyleMari <kylemaritorralba75@gmail.com>
2026-03-17 14:57:46 +0000

    Creation of MongoDB database and Django project

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(name='Bruce Wayne', email='bruce@wayne.com', team='dc')
+        self.assertEqual(user.name, 'Bruce Wayne')
+        self.assertEqual(user.team, 'dc')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='marvel')
+        self.assertEqual(team.name, 'marvel')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        activity = Activity.objects.create(user='Bruce Wayne', activity_type='Running', duration=30, date='2026-03-17')
+        self.assertEqual(activity.activity_type, 'Running')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        lb = Leaderboard.objects.create(team='marvel', points=100)
+        self.assertEqual(lb.team, 'marvel')
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Do pushups', difficulty='Easy')
+        self.assertEqual(workout.name, 'Pushups')
